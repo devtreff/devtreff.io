c62ea4fe7848cf64cf07cfe81363e1de226da9a8
Gerald Urschitz <gerald.urschitz@gmail.com>
2019-08-10 09:24:34 +0200

    add speaker tiles to editions

---
+++ src/components/SpeakerTile.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="md:w-1/3 pt-4 md:pt-0 md:px-4">
+    <img
+      class="h-64 md:h-48 lg:h-64"
+      :src="
+        speaker.image
+          ? speaker.image
+          : 'https://devtreff.io/src/img/webp/dummy.webp'
+      "
+    />
+    <h6 class="font-bold text-lg mt-3">{{ speaker.name }}</h6>
+    <div>
+      {{ speaker.position }}
+    </div>
+    <div>
+      {{ speaker.company }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    speaker: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/templates/Edition.vue
+++ src/templates/Edition.vue
@@ -7,6 +7,15 @@ query Edition($path: String!) {
       content{
         hero_image
         date
+        speaker{
+          _uid
+          name
+          image
+          topic_title
+          topic_subtitle
+          position
+          company
+        }
         agenda{
           _uid
           time
@@ -55,21 +64,36 @@ query Edition($path: String!) {
             Am
             <FormatDate :date-string="edition.nextEvent.content.date" />
           </div>
-          <h1 class="mt-4 text-2xl md:text-4xl lg:text-5xl font-bold">{{ edition.nextEvent.name }}</h1>
+          <h1 class="mt-4 text-2xl md:text-4xl lg:text-5xl font-bold">
+            {{ edition.nextEvent.name }}
+          </h1>
         </div>
       </div>
     </template>
     <template #main>
       <section
-        v-if="hasNextEvent"
-        class="relative flex pt-8 pb-8 lg:py-16 items-center justify-end overflow-hidden flex-col lg:flex-row bg-gray-200"
+        v-if="nextEventHasSpeakers"
+        class="relative flex pt-8 pb-8 lg:py-16 items-center justify-end overflow-hidden flex-col bg-gray-200"
       >
-        <div class="lg:max-w-2xl w-full px-5 z-10 bg-gray-200 lg:pr-12">
+        <div class="lg:max-w-4xl w-full px-5 z-10 bg-gray-200">
           <div class="mb-8">
             <Title class="lg:text-xl">Die Speaker</Title>
           </div>
         </div>
-        <div :style="{ 'max-width': '500px' }" class="flex-1"></div>
+        <div class="max-w-4xl md:flex md:justify-center">
+          <SpeakerTile
+            v-for="speaker in edition.nextEvent.content.speaker"
+            :key="speaker._uid"
+            :speaker="speaker"
+          />
+        </div>
+        <div class="mt-10">
+          <a
+            class="bg-gray-900 hover:bg-gray-800 text-white text-center px-6 py-1 inline-flex font-bold"
+            href="mailto:contact@devtreff.io"
+            >Bewirb dich als Speaker</a
+          >
+        </div>
       </section>
       <section
         v-else
@@ -91,7 +115,8 @@ query Edition($path: String!) {
           <a
             class="bg-gray-900 hover:bg-gray-800 text-white text-center px-6 py-1 inline-flex font-bold"
             href="mailto:contact@devtreff.io"
-          >Bewirb dich als Speaker</a>
+            >Bewirb dich als Speaker</a
+          >
         </div>
         <div :style="{ 'max-width': '500px' }" class="flex-1"></div>
       </section>
@@ -135,7 +160,10 @@ query Edition($path: String!) {
           <p class="text-gray-600">{{ location.content.city }}</p>
           <h1 class="text-4xl font-bold">{{ location.content.title }}</h1>
           <div class="whitespace-pre-line leading-relaxed">
-            <vue-simple-markdown class="markdown-container" :source="location.content.description"></vue-simple-markdown>
+            <vue-simple-markdown
+              class="markdown-container"
+              :source="location.content.description"
+            ></vue-simple-markdown>
           </div>
           <p class="text-gray-600 pt-5">
             {{ location.content.street }}, {{ location.content.postcode }}
@@ -144,7 +172,10 @@ query Edition($path: String!) {
         </div>
         <div :style="{ 'max-width': '500px' }" class="flex-1"></div>
       </section>
-      <Map :lat="parseFloat(location.content.lat)" :lng="parseFloat(location.content.long)" />
+      <Map
+        :lat="parseFloat(location.content.lat)"
+        :lng="parseFloat(location.content.long)"
+      />
     </template>
   </FullImageLayout>
 </template>
@@ -154,13 +185,15 @@ import FormatDate from "../components/FormatDate.vue";
 import Title from "../components/Title.vue";
 import Agenda from "../components/Agenda.vue";
 import Map from "../components/Map.vue";
+import SpeakerTile from "../components/SpeakerTile.vue";
 
 export default {
   components: {
     Title,
     FormatDate,
     Agenda,
-    Map
+    Map,
+    SpeakerTile
   },
   computed: {
     edition() {
@@ -171,6 +204,11 @@ export default {
     },
     hasNextEvent() {
       return !!this.edition.nextEvent;
+    },
+    nextEventHasSpeakers() {
+      return this.hasNextEvent
+        ? this.edition.nextEvent.content.speaker.length > 0
+        : false;
     },
     coordinates() {
       return {
