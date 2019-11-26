a80c66c750656e8a6b69bbc775d2df96a7318581
Gerald Urschitz <gerald.urschitz@bestit-online.at>
2019-11-26 15:22:08 +0100

    fix typo in section title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -66,7 +66,10 @@ query{
 </static-query>
 
 <template>
-  <FullImageLayout :has-gradient="true" image="//img2.storyblok.com/1024x600/f/59562/2048x1369/d54c0aa61b/76765482_474220090114834_8385444739406626816_o_474220086781501.jpg">
+  <FullImageLayout
+    :has-gradient="true"
+    image="//img2.storyblok.com/1024x600/f/59562/2048x1369/d54c0aa61b/76765482_474220090114834_8385444739406626816_o_474220086781501.jpg"
+  >
     <template #hero>
       <div class="flex-1 flex justify-center items-center">
         <div class="max-w-4xl w-full mt-5 lg:mt-0">
@@ -125,7 +128,7 @@ query{
 
         <div class="z-20 max-w-xl mx-auto relative">
           <Title class="text-lg md:text-xl lg:text-4xl"
-            >Vergangene DevTreff</Title
+            >Vergangene DevTreffs</Title
           >
         </div>
 
