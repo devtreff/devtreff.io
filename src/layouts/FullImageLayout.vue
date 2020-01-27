44c37e172d8de5e0db18008ce0a4581ea5144c44
Mirco Baumann <mirco.baumann@bestit-online.de>
2020-01-27 11:05:40 +0100

    closes #37 » Center Container & some frontend fixes

--- src/layouts/FullImageLayout.vue
+++ src/layouts/FullImageLayout.vue
@@ -1,142 +1,146 @@
 <template>
-  <div>
-    <FullImage
-      :has-gradient="hasGradient"
-      :has-vertical-gradient="hasVerticalGradient"
-      :alt="alt"
-      :src="image"
-    >
-      <template #content>
-        <div class="h-full container mx-auto px-4 pt-0 md:pt-4 lg:pt-6 xl:pt-8">
-          <div class="flex flex-col h-full">
-            <Header />
-            <slot name="hero" />
-          </div>
-        </div>
-      </template>
-    </FullImage>
-    <main class="main text-dark">
-      <slot name="main" />
-    </main>
-    <footer class="bg-gray-900 text-white py-12 z-10 relative">
-      <div class="w-full flex justify-center">
-        <div class="w-48 pb-12">
-          <img
-            alt="Das Logo von best it eCommerce solutions"
-            src="/images/best-it-logo.png"
-          />
-        </div>
-      </div>
-      <hr class="bg-gray-500 opacity-75 h-px max-w-4xl" />
-      <div class="font-bold flex flex-wrap justify-center text-xs pt-5">
-        <a href="#" class="nav__link my-2" @click="showImprintModal"
-          >Impressum</a
+    <div>
+        <FullImage
+                :has-gradient="hasGradient"
+                :has-vertical-gradient="hasVerticalGradient"
+                :alt="alt"
+                :src="image"
         >
-        <g-link class="nav__link my-2" to="/amstetten"
-          >DevTreff:Amstetten</g-link
+            <template #content>
+                <div class="h-full container mx-auto px-4 pt-0 md:pt-4 lg:pt-6 xl:pt-8">
+                    <div class="flex flex-col h-full">
+                        <Header/>
+                        <slot name="hero"/>
+                    </div>
+                </div>
+            </template>
+        </FullImage>
+        <main class="main text-dark">
+            <slot name="main"/>
+        </main>
+        <footer class="bg-gray-900 text-white py-12 z-10 relative">
+            <div class="w-full flex justify-center">
+                <div class="w-48 pb-12">
+                    <img
+                            alt="Das Logo von best it eCommerce solutions"
+                            src="/images/best-it-logo.png"
+                    />
+                </div>
+            </div>
+            <hr class="container mx-auto bg-gray-500 opacity-75 h-px max-w-4xl"/>
+            <div class="font-bold flex flex-wrap justify-center text-xs pt-5">
+                <a href="#" class="nav__link my-2" @click="showImprintModal"
+                >Impressum
+                </a
+                >
+                <g-link class="nav__link my-2" to="/amstetten"
+                >DevTreff:Amstetten
+                </g-link
+                >
+                <g-link class="nav__link my-2" to="/muensterland"
+                >DevTreff:Münsterland
+                </g-link
+                >
+                <g-link class="nav__link my-2" to="/siegburg">DevTreff:Siegburg</g-link>
+            </div>
+            <div class="flex flex-wrap justify-center mt-4">
+                <a href="https://github.com/devtreff/devtreff.io" target="_blank">
+                    <font-awesome-icon class="h-8 w-8" :icon="['fab', 'github']"/>
+                </a>
+            </div>
+        </footer>
+        <modal
+                name="imprint"
+                width="100%"
+                height="100%"
+                classes="md:max-w-4xl bg-white mx-auto md:h-96 my-auto"
         >
-        <g-link class="nav__link my-2" to="/muensterland"
-          >DevTreff:Münsterland</g-link
-        >
-        <g-link class="nav__link my-2" to="/siegburg">DevTreff:Siegburg</g-link>
-      </div>
-      <div class="flex flex-wrap justify-center mt-4">
-        <a href="https://github.com/devtreff/devtreff.io" target="_blank">
-          <font-awesome-icon class="h-8 w-8" :icon="['fab', 'github']" />
-        </a>
-      </div>
-    </footer>
-    <modal
-      name="imprint"
-      width="100%"
-      height="100%"
-      classes="md:max-w-4xl bg-white mx-auto md:h-96 my-auto"
-    >
-      <div class="text-gray-900 p-5">
-        <h3 class="text-3xl font-bold">Impressum</h3>
-        <p class="mt-5">
-          <strong>best it E-Commerce GmbH</strong>
-          <br />Linzer Straße 12 <br />3300 Amstetten
-        </p>
-        <p class="mt-5">
-          Telefon: +43 7472 909 00
-          <br />E-Mail:
-          <a href="mailto:info@bestit-online.at">info@bestit-online.at</a>
-        </p>
-        <a class="block mt-8 underline" href="#" @click="closeImprintModal"
-          >Schließen</a
-        >
-      </div>
-    </modal>
-  </div>
+            <div class="text-gray-900 p-5">
+                <h3 class="text-3xl font-bold">Impressum</h3>
+                <p class="mt-5">
+                    <strong>best it E-Commerce GmbH</strong>
+                    <br/>Linzer Straße 12 <br/>3300 Amstetten
+                </p>
+                <p class="mt-5">
+                    Telefon: +43 7472 909 00
+                    <br/>E-Mail:
+                    <a href="mailto:info@bestit-online.at">info@bestit-online.at</a>
+                </p>
+                <a class="block mt-8 underline" href="#" @click="closeImprintModal"
+                >Schließen
+                </a
+                >
+            </div>
+        </modal>
+    </div>
 </template>
 
 <static-query>
-query {
-  metadata {
+    query {
+    metadata {
     siteName
     coverImage
-  }
-}
+    }
+    }
 </static-query>
 
 <script lang="ts">
-import Vue from "vue";
-import Header from "../components/Header.vue";
-import FullImage from "../components/FullImage.vue";
+    import Vue from "vue";
+    import Header from "../components/Header.vue";
+    import FullImage from "../components/FullImage.vue";
 
-export default Vue.extend({
-  components: {
-    Header,
-    FullImage
-  },
-  props: {
-    image: {
-      type: String,
-      required: true
-    },
-    hasVerticalGradient: {
-      type: Boolean,
-      default: false
-    },
-    hasGradient: {
-      type: Boolean,
-      default: false
-    },
-    alt: {
-      type: String,
-      required: true
-    }
-  },
-  methods: {
-    showImprintModal() {
-      this.$modal.show("imprint");
-    },
-    closeImprintModal() {
-      this.$modal.hide("imprint");
-    }
-  }
-});
+    export default Vue.extend({
+        components: {
+            Header,
+            FullImage
+        },
+        props: {
+            image: {
+                type: String,
+                required: true
+            },
+            hasVerticalGradient: {
+                type: Boolean,
+                default: false
+            },
+            hasGradient: {
+                type: Boolean,
+                default: false
+            },
+            alt: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            showImprintModal() {
+                this.$modal.show("imprint");
+            },
+            closeImprintModal() {
+                this.$modal.hide("imprint");
+            }
+        }
+    });
 </script>
 
 <style>
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 80px;
-}
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 80px;
+    }
 
-.nav__link {
-  margin-left: 20px;
-}
+    .nav__link {
+        margin-left: 20px;
+    }
 
-.container {
-  z-index: 20;
-}
+    .container {
+        z-index: 20;
+    }
 
-.logo {
-  width: 20px;
-  height: 20px;
-}
+    .logo {
+        width: 20px;
+        height: 20px;
+    }
 </style>
--- src/layouts/Section.vue
+++ src/layouts/Section.vue
@@ -1,96 +1,97 @@
 <template>
-  <section
-    :class="[
-      'relative flex pt-8 lg:py-16 items-center justify-end overflow-hidden',
+    <section
+            :class="[
+      'container mx-auto relative flex pt-8 lg:py-16 items-center justify-end overflow-hidden',
       isReversed ? 'flex-col lg:flex-row-reverse' : 'flex-col lg:flex-row',
       showWhiteBackground ? 'bg-white' : 'bg-gray-100'
     ]"
-  >
-    <img
-      v-if="!isReversed && showDots"
-      class="absolute"
-      role="presentation"
-      alt
-      src="../images/dots_small.svg"
-      :style="{ left: '-50px', top: 0 }"
-    />
-    <img
-      v-if="isReversed && showDots"
-      class="absolute"
-      role="presentation"
-      alt
-      src="../images/dots_small.svg"
-      :style="{ right: '-50px', top: '-150px' }"
-    />
-    <div
-      :class="[
+    >
+        <img
+                v-if="!isReversed && showDots"
+                class="absolute"
+                role="presentation"
+                alt
+                src="../images/dots_small.svg"
+                :style="{ left: '-50px', top: 0 }"
+        />
+        <img
+                v-if="isReversed && showDots"
+                class="absolute"
+                role="presentation"
+                alt
+                src="../images/dots_small.svg"
+                :style="{ right: '-50px', top: '-150px' }"
+        />
+
+        <div
+                :class="[
         'lg:max-w-2xl w-full px-5 z-10',
         isReversed ? 'lg:pl-12 bg-white' : 'lg:pr-12',
         showWhiteBackground ? 'bg-white' : 'bg-gray-100'
       ]"
-    >
-      <div class="mb-8">
-        <Title class="lg:text-xl">{{ title }}</Title>
-      </div>
-      <div class="whitespace-pre-line leading-relaxed">
-        <vue-simple-markdown class="markdown-container" :source="description"></vue-simple-markdown>
-      </div>
-    </div>
-    <div v-if="image" class="mt-6 lg:mt-0 z-10">
-      <v-lazy-image :src="image" :alt="imageAltText" />
-    </div>
-    <div v-else class="image-placeholder flex-1" />
-  </section>
+        >
+            <div class="container mx-auto mb-8">
+                <Title class="lg:text-xl">{{ title }}</Title>
+            </div>
+            <div class="whitespace-pre-line leading-relaxed">
+                <vue-simple-markdown class="markdown-container" :source="description"></vue-simple-markdown>
+            </div>
+        </div>
+        <div v-if="image" class="m-6 lg:mt-0 z-10">
+            <v-lazy-image :src="image" :alt="imageAltText"/>
+        </div>
+        <div v-else class="image-placeholder flex-1"/>
+    </section>
 </template>
 
 <script>
-import Title from "../components/Title.vue";
+    import Title from "../components/Title.vue";
 
-export default {
-  components: {
-    Title
-  },
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    image: {
-      type: String,
-      required: false,
-      default: null
-    },
-    imageAltText: {
-      type: String,
-      required: false,
-      default: null
-    },
-    isReversed: Boolean,
-    hasWhiteBackground: {
-      type: Boolean,
-      default: undefined
-    },
-    showDots: Boolean
-  },
-  computed: {
-    showWhiteBackground() {
-      return this.hasWhiteBackground === undefined
-        ? !this.isReversed
-        : this.hasWhiteBackground;
-    }
-  }
-};
+    export default {
+        components: {
+            Title
+        },
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: false,
+                default: null
+            },
+            imageAltText: {
+                type: String,
+                required: false,
+                default: null
+            },
+            isReversed: Boolean,
+            hasWhiteBackground: {
+                type: Boolean,
+                default: undefined
+            },
+            showDots: Boolean
+        },
+        computed: {
+            showWhiteBackground() {
+                return this.hasWhiteBackground === undefined
+                        ? !this.isReversed
+                        : this.hasWhiteBackground;
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-img,
-.image-placeholder {
-  @media (min-width: theme("screens.lg")) {
-    max-width: 500px;
-  }
-}
+    img,
+    .image-placeholder {
+        @media (min-width: theme("screens.lg")) {
+            max-width: 500px;
+        }
+    }
 </style>
